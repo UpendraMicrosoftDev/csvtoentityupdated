820acafd2cc08074b3fd6de8b8838b0bcc144f3e
upendraigate <upendra.engr@gmail.com>
2019-02-18 09:00:16 +1100

    commit

---
+++ DataLayer/CsvFieldAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportLibrary
+{
+    public class CsvFieldAttribute : Attribute
+    {
+       
+        public int CsvFieldLocation { get; set; }
+
+        public CsvFieldAttribute(int location)
+        {
+            CsvFieldLocation = location;
+        }
+
+
+    }
+}
---
+++ DataLayer/Output/SqlRepository.cs
@@ -0,0 +1,53 @@
+﻿using CSVToEntity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace DataLayer.Output
+{
+    public class SqlRepository<T> : IDataRepository<T> where T:class
+    {
+        private StudentTestCSVEntities _dbContext;
+        private IDbSet<T> _entity;
+
+        public SqlRepository(StudentTestCSVEntities dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool UpdateData(IEnumerable<T> entity)
+        {
+            try
+            {
+                foreach(var tempEntity in entity)
+                    Entities.Add(tempEntity);
+
+                _dbContext.SaveChanges();
+
+                return true;
+                
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                _dbContext.Dispose();
+            }
+
+        }
+
+        private IDbSet<T> Entities
+        {
+            get
+            {
+                if (_entity == null)
+                    _entity = _dbContext.Set<T>();
+
+                return _entity;
+            }
+        }
+
+    }
+}
